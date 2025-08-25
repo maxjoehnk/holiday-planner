2cc6612d33f87178c949adc059eb276522e40740
Max Jöhnk <maxjoehnk@gmail.com>
2025-08-25 20:21:29 +0200

    feat: add version number to settings

--- lib/views/settings/settings_view.dart
+++ lib/views/settings/settings_view.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:holiday_planner/settings.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   const SettingsView({
     super.key,
   });
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  final Future<PackageInfo> _packageInfoFuture = PackageInfo.fromPlatform();
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +63,26 @@ class SettingsView extends StatelessWidget {
                     settings.updateThemeMode(val);
                   }
                 },
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16, 24, 16, 8),
+                child: Text(
+                  'About',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: FutureBuilder(
+                    future: _packageInfoFuture,
+                    builder: (context, asyncSnapshot) {
+                      if (!asyncSnapshot.hasData) {
+                        return Container();
+                      }
+                      return Text(
+                          'Holiday Planner App ${asyncSnapshot.requireData.version}',
+                          style: Theme.of(context).textTheme.bodyMedium);
+                    }),
               ),
             ],
           ),
